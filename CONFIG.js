3199cc916b7c3496cda1c3471447eb579a1aa300
thwgtho <95741812+thwgtho@users.noreply.github.com>
2021-12-11 21:29:19 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -4,7 +4,7 @@ const CONFIGDATA = {
     "Bạn đã hết thời gian dùng thử gói nguoiiu trong 4M. Bạn có muốn đổi sang gói nguoiiu premium không? Gói premium có thời hạn là mãi mãi và trả bằng nhiều cái hun hun ",
   buttonYes: "Đồng ý",
   buttonNo: "Sao cũng được",
-  titleModar: "Chúc mừng bạn đã nhận được gói premium. Để kích hoạt vui lòng quay lại mess gửi "Yêu chị" cho NTT",
+  titleModar: "Chúc mừng bạn đã nhận được gói premium. Để kích hoạt vui lòng quay lại mess gửi "Yeu chi" cho NTT",
   descriptionModar:
     ' Với bản premium này bạn có thể nhận được người ôm ôm, hun hun, trông bạn ngủ, chạy deadline cùng và iu bạn nhìu nhìu </br>  Cám ơn bạn đã sử dụng dịch vụ </br>  Nếu bạn hài lòng về sản phẩm hãy cho chúng tôi 5*',
 };
